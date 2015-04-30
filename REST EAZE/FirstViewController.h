367c9938e09108a35b993850577c2fee0195f206
Amitanshu Jha <amitanshuoranuj@gmail.com>
2015-04-30 14:10:17 -0400

    RestEaze code
    started with build 36

--- REST EAZE/FirstViewController.h
+++ REST EAZE/FirstViewController.h
@@ -7,9 +7,29 @@
 //
 
 #import <UIKit/UIKit.h>
+#import "sqlite3.h"
 
 @interface FirstViewController : UIViewController
+@property (strong, nonatomic) IBOutlet UIScrollView *scrollView;
+@property (nonatomic) IBOutlet UILabel *headerLabel;
+@property (strong, nonatomic) NSString* username;
 
+//- (IBAction)downloadFromServer:(id)sender;
+//- (IBAction)loadData:(id)sender;
+
+//-(void) writeToTextFile;
+//-(void) readFromTextFile;
+//- (void) readFileFromServerAndStoreInMemory;
+//- (void) deleteStoredFile;
+//- (void) uploadFileToServer;
+
+@property (nonatomic) UIButton *prevBtn;
+@property (nonatomic) UIButton *nextBtn;
+
+@property (nonatomic) UIButton *refreshBtn;
+//database
+@property (strong, nonatomic) NSString *databasePath;
+@property (nonatomic) sqlite3 *myDataBase;
 
 @end
 
